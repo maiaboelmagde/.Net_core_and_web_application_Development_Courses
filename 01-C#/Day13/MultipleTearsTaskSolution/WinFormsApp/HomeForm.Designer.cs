8d50cf5a71e2f7bfe0b3d9c4b71e9972635aecb4
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-02-24 10:07:09 +0200

    adding HomeForm ...

---
+++ 01-C#/Day13/MultipleTearsTaskSolution/WinFormsApp/HomeForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormsApp
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            GridView = new Button();
+            DetailedList = new Button();
+            SuspendLayout();
+            // 
+            // GridView
+            // 
+            GridView.Location = new Point(150, 66);
+            GridView.Name = "GridView";
+            GridView.Size = new Size(486, 100);
+            GridView.TabIndex = 0;
+            GridView.Text = "GridView Page";
+            GridView.UseVisualStyleBackColor = true;
+            GridView.Click += GridView_Click;
+            // 
+            // DetailedList
+            // 
+            DetailedList.Location = new Point(150, 215);
+            DetailedList.Name = "DetailedList";
+            DetailedList.Size = new Size(486, 100);
+            DetailedList.TabIndex = 1;
+            DetailedList.Text = "DetailedList Page";
+            DetailedList.UseVisualStyleBackColor = true;
+            DetailedList.Click += DetailedList_Click;
+            // 
+            // HomeForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(DetailedList);
+            Controls.Add(GridView);
+            Name = "HomeForm";
+            Text = "HomeForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button GridView;
+        private Button DetailedList;
+    }
+}
---
+++ 01-C#/Day13/MultipleTearsTaskSolution/WinFormsApp/HomeForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GridView_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void DetailedList_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
