f0f1335f01274534d55dadc11b96aa37c637e80c
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-03-22 06:56:42 +0200

    Add Student Constrains

--- 02-API/APILabs/APILabs/Models/Student.cs
+++ 02-API/APILabs/APILabs/Models/Student.cs
@@ -1,13 +1,25 @@
-﻿namespace APILabs.Models
+﻿using System.ComponentModel.DataAnnotations;
+using APILabs.Validation;
+
+namespace APILabs.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [MinLength(3 , ErrorMessage = "Name must be at least 3 letters")]
+        [MaxLength(10, ErrorMessage = "Name can't exceed 10 letters")]
         public string Name { get; set; }
+
+        [RegularExpression(@"^(.*\.jpg|.*\.png)$", ErrorMessage = "Image must end with .jpg or .png")]
         public string Image { get; set; }
         public string address { get; set; }
+
+        [Range(18, 22 , ErrorMessage ="Allowed age is between 18 and 22 !!")]
         public int age { get; set; }
+        [BirthDateValidation(18, 22)]
         public DateTime birthDate { get; set; }
+        [RegularExpression(@"^\d+$", ErrorMessage = "Phone number must contain only digits.")]
         public string PhoneNum { get; set; }
         public int grade { get; set; }
     }
---
+++ 02-API/APILabs/APILabs/Validation/BirthDateValidationAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APILabs.Validation
+{
+    public class BirthDateValidationAttribute : ValidationAttribute
+    {
+        private readonly int _minAge;
+        private readonly int _maxAge;
+
+        public BirthDateValidationAttribute(int minAge, int maxAge)
+        {
+            _minAge = minAge;
+            _maxAge = maxAge;
+        }
+
+
+        protected override ValidationResult IsValid(object? value, ValidationContext context)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Birth date is required.");
+            }
+
+            if (value is DateTime birthDate)
+            {
+                var today = DateTime.Today;
+                int age = today.Year - birthDate.Year;
+
+                if (birthDate > today.AddYears(-age))
+                    age--;
+
+                if (age < _minAge || age > _maxAge)
+                {
+                    return new ValidationResult($"Allowed age is between {_minAge} and {_maxAge} based on birth date.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
