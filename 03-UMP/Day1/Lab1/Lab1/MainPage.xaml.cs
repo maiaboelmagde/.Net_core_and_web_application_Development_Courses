46f567c44305d318e789938310194a5c17f97df3
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-04-11 16:20:33 +0200

    The 1st day tasks solutions

---
+++ 03-UMP/Day1/Lab1/Lab1/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CalcAge(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool validInput = int.TryParse(txtBox1.Text, out int birthYear);
+                if (validInput)
+                {
+                    int currentYear = DateTime.Now.Year;
+                    int age = currentYear - birthYear;
+                    if(currentYear < birthYear)
+                    {
+                        throw new Exception("Invalid Year !!");
+                    }
+                    resultTXT.Text = age.ToString();
+                    resultTXT.Foreground = new SolidColorBrush(Windows.UI.Colors.Black);
+                    resultTXT.FontSize = 50;
+                }
+                else
+                {
+                    resultTXT.Text = "Invalid Input !!!!";
+                    resultTXT.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                    resultTXT.FontSize = 30;
+                }
+            }catch(Exception ex)
+            {
+                resultTXT.Text = "Error : "+ex.ToString();
+                resultTXT.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+                resultTXT.FontSize = 15;
+            }
+        }
+    }
+}
---
+++ 03-UMP/Day1/README.md
@@ -0,0 +1,8 @@
+**The 1st lab Tasks** : 
+
+***Task1 :***
+Calculate age based on birth year..
+<video controls src="TasksRecording/20250411-1407-01.5045074.mp4" title="Title"></video>
+***Task2 :***\
+Show Grid cells with diff colors....
+![alt text](TasksRecording/image.png)
