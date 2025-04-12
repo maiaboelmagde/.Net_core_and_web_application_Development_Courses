5a195fab4ee1b10feab596249afe8026bb4b575c
maiaboelmagde <102434408+maiaboelmagde@users.noreply.github.com>
2025-04-12 15:19:17 +0200

    The 2nd day tasks solutions

---
+++ 03-UMP/Day2/Task2/Task2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Age {  get; set; }
+        public double Salary {  get; set; }
+    }
+}
+
